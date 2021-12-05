7362001989debb9377372a498aafb590667412c0
piotrkrzyszowski <piotrek.krzyszowski@gmail.com>
2021-12-05 12:27:15 +0100

    Initial commit

---
+++ src/test/java/com/example/botdiscord/BotDiscordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.botdiscord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotDiscordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
